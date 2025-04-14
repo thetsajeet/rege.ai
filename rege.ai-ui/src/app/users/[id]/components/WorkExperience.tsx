12d1e10ef6efaa3754b985db90ee9fb0f99d42ea
T S Ajeet <ajeet.tumuluru@gmail.com>
2025-04-14 11:12:38 +0530

    modal setup modular

--- rege.ai-ui/src/app/users/[id]/components/WorkExperience.tsx
+++ rege.ai-ui/src/app/users/[id]/components/WorkExperience.tsx
@@ -1,60 +1,77 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogOverlay, DialogTrigger, DialogContent, DialogTitle, DialogDescription, DialogHeader, DialogClose } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogOverlay,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+  DialogHeader,
+  DialogClose,
+} from "@/components/ui/dialog";
 import { Edit, Pencil, Trash, X } from "lucide-react";
 import EditExperience from "./EditExperienceItem";
 import { useState } from "react";
 import EditDialog from "@/components/shared/EditDialog";
 
 function ExperienceItem() {
-  const [modalOpen, setModalOpen] = useState<boolean>(false);
-
   return (
     <>
       <div className="space-y-1 overflow-hidden">
         <div className="flex justify-start items-center gap-2 relative">
-          <EditDialog trigger={
+          <EditDialog
+            trigger={
               <Pencil className="size-4 text-yellow-400 cursor-pointer" />
-          } content={
-            <DialogHeader>
-                <DialogTitle>Are you absolutely sure?</DialogTitle>
-                <DialogDescription>
-                  editting
-                </DialogDescription>
+            }
+            content={
+              <DialogHeader>
+                <DialogTitle>Editing exp-1</DialogTitle>
+                <DialogDescription>editting</DialogDescription>
               </DialogHeader>
-          } />
-          <EditDialog trigger={
-              <Trash className="size-4 text-red-400 cursor-pointer" />
-          } content={
-            <DialogHeader>
-                <DialogTitle>Are you absolutely sure?</DialogTitle>
-                <DialogDescription>
-                  This action cannot be undone. This will permanently delete your account
-                  and remove your data from our servers.
-                </DialogDescription>
+            }
+            close={
+              <div className="flex justify-start gap-2">
+                <Button variant="outline" className="cursor-pointer">
+                  Cancel
+                </Button>
+                <Button variant="destructive" className="cursor-pointer">
+                  Yes
+                </Button>
+              </div>
+            }
+          />
+          <EditDialog
+            trigger={<Trash className="size-4 text-red-400 cursor-pointer" />}
+            content={
+              <DialogHeader>
+                <DialogTitle>Are you sure?</DialogTitle>
+                <DialogDescription>editting</DialogDescription>
               </DialogHeader>
-          } />
-
+            }
+            close={
+              <div className="flex justify-start gap-2">
+                <Button variant="outline" className="cursor-pointer">
+                  Cancel
+                </Button>
+                <Button variant="destructive" className="cursor-pointer">
+                  Yes
+                </Button>
+              </div>
+            }
+          />
         </div>
         <div className="flex justify-between items-start">
           <div className="flex gap-1">
-            <span >
-              sde
-            </span>
+            <span>sde</span>
             <span>&middot;</span>
-            <span >
-              citi
-            </span>
+            <span>citi</span>
           </div>
           <p className="flex italic gap-1 text-sm text-zinc-500 dark:text-zinc-400 relative">
-            <span >
-              Jan 2023
-            </span>
+            <span>Jan 2023</span>
             <span>-</span>
-            <span >
-              Jan 2023
-            </span>
+            <span>Jan 2023</span>
           </p>
         </div>
 
@@ -63,7 +80,6 @@ function ExperienceItem() {
           <li>Implemented shadcn UI components for rapid prototyping</li>
           <li>Integrated LLM-based JD parsing and resume matching</li>
         </ul>
-
       </div>
 
       <hr className="border-zinc-300 dark:border-zinc-800" />
@@ -89,13 +105,12 @@ export default function WorkExperience() {
         </div>
 
         <hr className="border-zinc-400 dark:border-zinc-700" />
-        
-          <div className="space-y-6">
-            {[1, 2].map((item: any, key: any) => (
-              <ExperienceItem key={key}/>
-            ))}
-          </div>
-        
+
+        <div className="space-y-6">
+          {[1, 2].map((item: any, key: any) => (
+            <ExperienceItem key={key} />
+          ))}
+        </div>
 
         <button className="w-full mt-4 text-sm text-white bg-purple-600 hover:bg-purple-700 py-2 rounded-md transition">
           + Add New Experience
--- rege.ai-ui/src/components/shared/EditDialog.tsx
+++ rege.ai-ui/src/components/shared/EditDialog.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import { useState } from "react"
-import { Dialog, DialogTitle, DialogTrigger, DialogContent, DialogHeader, DialogDescription } from "../ui/dialog"
+import { Dialog, DialogTitle, DialogTrigger, DialogContent, DialogHeader, DialogDescription, DialogFooter, DialogClose } from "../ui/dialog"
 
-export default function EditDialog({trigger, content}: any) {
+export default function EditDialog({trigger, content, close}: any) {
     const [modalOpen, setModalOpen] = useState<boolean>(false)
 
     return (
@@ -14,6 +14,11 @@ export default function EditDialog({trigger, content}: any) {
             {modalOpen && <div className="fixed inset-0 bg-zinc-900/60 backdrop-blur-sm z-40 pointer-events-none" /> }
             <DialogContent>
                 {content}
+                <DialogFooter>
+                    <DialogClose asChild>
+                        {close}
+                    </DialogClose>
+                </DialogFooter>
             </DialogContent>
         </Dialog>
     )  
